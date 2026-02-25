63ccd0cec5f9feff721d8f6074d711f696b6d308
Egor Arutiunean <egor.arutiunean@gmail.com>
2026-02-26 00:53:55 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <iframe
+        className="site-frame"
+        src="/site/index.html"
+        title="Uprav Parking Site"
+      />
+    </main>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+﻿import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+const metrikaId = import.meta.env.VITE_METRIKA_ID
+
+if (metrikaId) {
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://mc.yandex.ru/metrika/tag.js?id=${metrikaId}`
+  document.head.appendChild(script)
+
+  const init = document.createElement('script')
+  init.text = `window.ym=window.ym||function(){(window.ym.a=window.ym.a||[]).push(arguments)};window.ym.l=Date.now();ym(${metrikaId},"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true});`
+  document.head.appendChild(init)
+}
+
+// TODO: GA support can be added via VITE_GA_ID when needed.
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
